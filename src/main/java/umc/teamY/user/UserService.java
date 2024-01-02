c88df7a90cd14793322e5a22a5edd64715ee015d
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-02 20:27:37 +0900

    feat : 회원 전체 조회 기능 추가

--- src/main/java/umc/teamY/user/UserService.java
+++ src/main/java/umc/teamY/user/UserService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.teamY.user.dto.UserListResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -11,5 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public List<UserListResponse> findAllUsers() {
+        log.info("유저 전체 조회");
+        //추후 요청한 유저 제외하고 조회하는 것으로 변경해야함
+        return userRepository.findAll().stream()
+                .map(user -> new UserListResponse(user.getId(), user.getName()))
+                .collect(Collectors.toList());
+    }
 }
