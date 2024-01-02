d8efd25e4f4be253863f39a1e13a9acab8b0f805
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-02 19:47:21 +0900

    feat : Tag 엔티티추가

--- src/main/java/umc/teamY/user/UserService.java
+++ src/main/java/umc/teamY/user/UserService.java
@@ -1,7 +1,15 @@
 package umc.teamY.user;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
 public class UserService {
+
+    private UserRepository userRepository;
 }
