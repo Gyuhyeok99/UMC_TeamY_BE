60d6fef460418d11a23221dbccf204118e177508
hajungIm <ihj_ewha@ewhain.net>
2024-01-02 16:46:20 +0900

    chore: Exception, Response 추가

---
+++ src/main/java/umc/teamY/exception/GlobalControllerAdvice.java
@@ -0,0 +1,21 @@
+package umc.teamY.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import umc.teamY.util.Response;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(SimpleSnsApplicationException.class)
+    public ResponseEntity<?> errorHandler(SimpleSnsApplicationException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error(e.getErrorCode().name()));
+    }
+
+}
+
--- src/main/java/umc/teamY/user/UserController.java
+++ src/main/java/umc/teamY/user/UserController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+//    @PostMapping("/join")
+//    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+//        return Response.success(UserJoinResponse.fromUser(userService.join(request.getName(), request.getPassword())));
+//    }
 }
