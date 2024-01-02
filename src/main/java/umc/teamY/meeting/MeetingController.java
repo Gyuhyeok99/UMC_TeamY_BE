2e95cd728392b8a79ee74a6dfb7fc05f8b02bee9
Eunse Jang <senuej37@gmail.com>
2024-01-02 21:38:42 +0900

    feat: Meeting 조회 구현

--- src/main/java/umc/teamY/meeting/MeetingController.java
+++ src/main/java/umc/teamY/meeting/MeetingController.java
@@ -1,9 +1,29 @@
 package umc.teamY.meeting;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import umc.teamY.meeting.dto.MeetingRequest;
+import umc.teamY.meeting.dto.MeetingTotalResponse;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/meeting")
 public class MeetingController {
+
+    private final MeetingService meetingService;
+
+    /** 회의 생성 */
+    @PostMapping("/")
+    public ResponseEntity<Void> createMeeting(@RequestBody MeetingRequest request) {
+        meetingService.createMeeting(request);
+        return ResponseEntity.ok().build();
+    }
+
+    /** 회의 전체 조회 */
+    @GetMapping("/{projectId}")
+    public ResponseEntity<MeetingTotalResponse> getTotalMeeting(@PathVariable("projectId") Long projectId) {
+        meetingService.getTotalMeeting(projectId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/umc/teamY/meeting/MeetingService.java
+++ src/main/java/umc/teamY/meeting/MeetingService.java
@@ -1,7 +1,38 @@
 package umc.teamY.meeting;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.teamY.exception.CustomException;
+import umc.teamY.meeting.dto.MeetingRequest;
+import umc.teamY.meeting.dto.MeetingTotalResponse;
+import umc.teamY.project.Project;
+import umc.teamY.project.ProjectRepository;
+
+import java.util.List;
+import static umc.teamY.exception.ErrorCode.PROJECT_NOT_EXIST;
 
 @Service
+@RequiredArgsConstructor
 public class MeetingService {
+
+    private final MeetingRepository meetingRepository;
+    private final ProjectRepository projectRepository;
+
+    /** 회의 생성 */
+    public void createMeeting (MeetingRequest request) {
+        Project project = projectRepository.findById(request.getProjectId())
+                .orElseThrow(() -> new CustomException(PROJECT_NOT_EXIST));
+
+        Meeting meeting = request.toEntity(project);
+        meetingRepository.save(meeting);
+    }
+
+    /** 전체 회의 조희 */
+    public MeetingTotalResponse getTotalMeeting(Long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new CustomException(PROJECT_NOT_EXIST));
+
+        List<Meeting> meetingList = meetingRepository.findMeetingsByProjectId(project.getId());
+        return new MeetingTotalResponse(meetingList);
+    }
 }
