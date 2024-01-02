22be27471e8a83d6d51e09dc34bbc696102a95bb
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-02 15:33:41 +0900

    chore : 프로젝트 초기 설정

---
+++ src/test/java/umc/teamY/TeamYApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.teamY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamYApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
