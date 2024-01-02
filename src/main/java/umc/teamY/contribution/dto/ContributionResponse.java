3182f5a09f3625e44fee843e2ecf9184279b7d79
Eunse Jang <senuej37@gmail.com>
2024-01-03 04:01:13 +0900

    feat : project 전체 todo 카운트

--- src/main/java/umc/teamY/contribution/dto/ContributionResponse.java
+++ src/main/java/umc/teamY/contribution/dto/ContributionResponse.java
@@ -3,6 +3,8 @@ package umc.teamY.contribution.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class ContributionResponse {
--- src/main/java/umc/teamY/todo/TodoRepository.java
+++ src/main/java/umc/teamY/todo/TodoRepository.java
@@ -1,9 +1,14 @@
 package umc.teamY.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findTodosByMeetingIdAndTagId(Long meetingId, Long tagId);
+
+    @Query("SELECT COUNT(t) FROM Todo t WHERE t.meeting.project.id = :projectId")
+    int countTodosByProjectId(@Param("projectId") Long projectId);
 }
