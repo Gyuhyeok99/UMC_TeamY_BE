b699e2665e166f50f69d5218360c11f9be335777
Eunse Jang <senuej37@gmail.com>
2024-01-02 23:39:56 +0900

    feat : tag ErrorCode 추가

--- src/main/java/umc/teamY/exception/ErrorCode.java
+++ src/main/java/umc/teamY/exception/ErrorCode.java
@@ -17,10 +17,10 @@ public enum ErrorCode {
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "User has invalid permission"),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Database error occurs"),
     NOTIFICATION_CONNECT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Connect to notification occurs error"),
-
-    // Meeting
+    
     PROJECT_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 프로젝트입니다."),
     MEETING_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 회의입니다."),
+    TAG_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 태그입니다."),
 
     USER_PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "Project not founded"),
     NOT_TO_MEET_NOT_FOUND(HttpStatus.NOT_FOUND, "Not To Meet not founded")
