2b9c8db4c43d2500236af78a7840527a18049aab
Eunse Jang <senuej37@gmail.com>
2024-01-02 23:40:13 +0900

    feat : 체크리스트 추가 로직 구현

--- src/main/java/umc/teamY/todo/TodoController.java
+++ src/main/java/umc/teamY/todo/TodoController.java
@@ -1,4 +1,25 @@
 package umc.teamY.todo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.teamY.global.Response;
+import umc.teamY.tag.TagService;
+import umc.teamY.todo.dto.TodoCreateRequest;
+import umc.teamY.todo.dto.TodoCreateResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/todo")
 public class TodoController {
+
+    private final TodoService todoService;
+
+    /** 체크리스트 추가 */
+    @PostMapping("/")
+    public Response<TodoCreateResponse> addTodoToMeeting(@RequestBody TodoCreateRequest request) {
+        return Response.success(todoService.addTodoMeeting(request));
+    }
 }
--- src/main/java/umc/teamY/todo/TodoService.java
+++ src/main/java/umc/teamY/todo/TodoService.java
@@ -1,6 +1,39 @@
 package umc.teamY.todo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import umc.teamY.exception.CustomException;
+import umc.teamY.meeting.Meeting;
+import umc.teamY.meeting.MeetingRepository;
+import umc.teamY.tag.Tag;
+import umc.teamY.tag.TagRepository;
+import umc.teamY.todo.dto.TodoCreateRequest;
+import umc.teamY.todo.dto.TodoCreateResponse;
 
+import static umc.teamY.exception.ErrorCode.MEETING_NOT_EXIST;
+import static umc.teamY.exception.ErrorCode.TAG_NOT_EXIST;
+
+@Service
+@RequiredArgsConstructor
 public class TodoService {
+
+    private final TodoRepository todoRepository;
+    private final MeetingRepository meetingRepository;
+    private final TagRepository tagRepository;
+
+    /** 체크리스트 생성 */
+    public TodoCreateResponse addTodoMeeting(TodoCreateRequest request) {
+        Meeting meeting = meetingRepository.findById(request.getMemberId())
+                .orElseThrow(() -> new CustomException(MEETING_NOT_EXIST));
+
+        Tag tag = tagRepository.findById(request.getTagId())
+                .orElseThrow(() -> new CustomException(TAG_NOT_EXIST));
+
+        Todo todo = request.toEntity(meeting, tag);
+        todoRepository.save(todo);
+
+        return new TodoCreateResponse(todo.getId());
+    }
+
 }
